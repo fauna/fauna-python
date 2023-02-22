27efa4824f6027f6c72027180f9dae3e0b4119b3
Darren Cunningham <cynicaljoy@gmail.com>
2023-02-22 15:01:04 -0500

    check fixups

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,6 +3,7 @@ import json
 from fauna import Client
 from fauna.client import QueryOptions
 
+
 def test_query():
     c = Client(secret="secret")
     q = """let foo = 'bar'
@@ -16,16 +17,16 @@ def test_query():
 
     assert as_json["data"] == 'bar'
 
+
 def test_query_with_opts():
-    c = Client(
-        secret="secret",
-        endpoint="http://localhost:8443",
-    )
-    res = c.query("Math.abs(-5.123e3)", QueryOptions(
-        tags="hello=world",
-        lineraized=True,
-        query_timeout_ms=5000,
-    ))
+    c = Client(secret="secret")
+    res = c.query(
+        "Math.abs(-5.123e3)",
+        QueryOptions(
+            tags="hello=world",
+            lineraized=True,
+            query_timeout_ms=5000,
+        ))
     # TODO: assert HTTP Client Request Headers contain expected values
     as_json = json.loads(res.read().decode("utf-8"))
     assert "error" not in as_json
