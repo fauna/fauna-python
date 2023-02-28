bdc13d3de7febe3163a2f23917c2aee0c989f127
Darren Cunningham <darren.cunningham@fauna.com>
2023-02-28 18:38:06 -0500

    using mapping

--- tests/unit/test_fingerprinting.py
+++ tests/unit/test_fingerprinting.py
@@ -1,63 +1,52 @@
+from typing import Mapping
 import os
 
 import fauna
 
 
 def test_fingerprinting(monkeypatch, subtests):
-    with subtests.test("netlify"):
-        monkeypatch.setenv("NETLIFY_IMAGES_CDN_DOMAIN", "sup")
+    tests: Mapping[str, Mapping[str, str]] = {
+        "Netlify": {
+            "NETLIFY_IMAGES_CDN_DOMAIN": "sup",
+        },
+        "Vercel": {
+            "VERCEL": "sup",
+        },
+        "Heroku": {
+            "PATH": ".heroku",
+        },
+        "AWS Lambda": {
+            "AWS_LAMBDA_FUNCTION_VERSION": "sup"
+        },
+        "GCP Cloud Functions": {
+            "_": "google",
+        },
+        "GCP Compute Instances": {
+            "GOOGLE_CLOUD_PROJECT": "sup",
+        },
+        "Azure Cloud Functions": {
+            "WEBSITE_FUNCTIONS_AZUREMONITOR_CATEGORIES": "sup"
+        },
+        "Azure Compute": {
+            "ORYX_ENV_TYPE": "AppService",
+            "WEBSITE_INSTANCE_ID": "sup",
+        },
+    }
 
-        assert fauna.headers._DriverEnvironment().env \
-            == "Netlify"
-        monkeypatch.delenv("NETLIFY_IMAGES_CDN_DOMAIN")
+    for t in tests:
+        with subtests.test(t):
+            # placeholder to reset PATH in case set within test (Heroku)
+            current = None
+            # set env vars for test
+            for e in tests[t]:
+                if e == "PATH":
+                    current = os.environ[e]
+                monkeypatch.setenv(e, tests[t][e])
 
-    with subtests.test("vercel"):
-        monkeypatch.setenv("VERCEL", "sup")
+            assert fauna.headers._DriverEnvironment().env == t
 
-        assert fauna.headers._DriverEnvironment().env \
-            == "Vercel"
-        monkeypatch.delenv("VERCEL")
-
-    with subtests.test("heroku"):
-        path = os.environ["PATH"]
-        monkeypatch.setenv("PATH", ".heroku")
-
-        assert fauna.headers._DriverEnvironment().env \
-            == "Heroku"
-        monkeypatch.setenv("PATH", path)
-
-    with subtests.test("AWS Lambda"):
-        monkeypatch.setenv("AWS_LAMBDA_FUNCTION_VERSION", "sup")
-
-        assert fauna.headers._DriverEnvironment().env \
-            == "AWS Lambda"
-        monkeypatch.delenv("AWS_LAMBDA_FUNCTION_VERSION")
-
-    with subtests.test("GCP Cloud Functions"):
-        monkeypatch.setenv("_", "google")
-
-        assert fauna.headers._DriverEnvironment().env \
-            == "GCP Cloud Functions"
-        monkeypatch.delenv("_")
-
-    with subtests.test("Google Compute Instances"):
-        monkeypatch.setenv("GOOGLE_CLOUD_PROJECT", "sup")
-        assert fauna.headers._DriverEnvironment().env \
-            == "GCP Compute Instances"
-        monkeypatch.delenv("GOOGLE_CLOUD_PROJECT")
-
-    with subtests.test("Azure Cloud Functions"):
-        monkeypatch.setenv("WEBSITE_FUNCTIONS_AZUREMONITOR_CATEGORIES", "sup")
-        assert fauna.headers._DriverEnvironment().env \
-            == "Azure Cloud Functions"
-        monkeypatch.delenv("WEBSITE_FUNCTIONS_AZUREMONITOR_CATEGORIES")
-
-    with subtests.test("Azure Compute"):
-        monkeypatch.setenv("ORYX_ENV_TYPE", "AppService")
-        monkeypatch.setenv("WEBSITE_INSTANCE_ID", "sup")
-
-        assert fauna.headers._DriverEnvironment().env \
-            == "Azure Compute"
-
-        monkeypatch.delenv("ORYX_ENV_TYPE")
-        monkeypatch.delenv("WEBSITE_INSTANCE_ID")
+            # clean up
+            for e in tests[t]:
+                if e == "PATH" and current is not None:
+                    os.environ[e] = current
+                monkeypatch.delenv(e)
