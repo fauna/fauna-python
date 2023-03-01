f3b736217baa4ec7b744fdc6941e7e1519e28eb0
Darren Cunningham <darren.cunningham@fauna.com>
2023-03-01 07:13:03 -0500

    Add tests to validate fingerprinting (#31)

---
+++ tests/unit/test_fingerprinting.py
@@ -0,0 +1,40 @@
+from typing import Mapping
+import fauna
+
+
+def test_fingerprinting(monkeypatch, subtests):
+    tests: Mapping[str, Mapping[str, str]] = {
+        "Netlify": {
+            "NETLIFY_IMAGES_CDN_DOMAIN": "sup",
+        },
+        "Vercel": {
+            "VERCEL": "sup",
+        },
+        "Heroku": {
+            "PATH": ".heroku",
+        },
+        "AWS Lambda": {
+            "AWS_LAMBDA_FUNCTION_VERSION": "sup"
+        },
+        "GCP Cloud Functions": {
+            "_": "google",
+        },
+        "GCP Compute Instances": {
+            "GOOGLE_CLOUD_PROJECT": "sup",
+        },
+        "Azure Cloud Functions": {
+            "WEBSITE_FUNCTIONS_AZUREMONITOR_CATEGORIES": "sup"
+        },
+        "Azure Compute": {
+            "ORYX_ENV_TYPE": "AppService",
+            "WEBSITE_INSTANCE_ID": "sup",
+        },
+    }
+
+    for t in tests:
+        with monkeypatch.context() as m:
+            with subtests.test(t):
+                for e in tests[t]:
+                    m.setenv(e, tests[t][e])
+
+                assert fauna.headers._DriverEnvironment().env == t
