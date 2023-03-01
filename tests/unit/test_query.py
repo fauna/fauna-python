9fc59cd37faf348041330488f0580b94e99cc342
Darren Cunningham <darren.cunningham@fauna.com>
2023-03-01 07:37:21 -0500

    moved query options test

--- tests/unit/test_query.py
+++ tests/unit/test_query.py
@@ -1,11 +1,6 @@
-from typing import Mapping
-
-import httpx
 import pytest
-from pytest_httpx import HTTPXMock
 
-from fauna import Client, Header, HTTPXClient, fql
-from fauna.client import QueryOptions
+from fauna import Client, fql
 from fauna.response import Stat
 
 
@@ -38,45 +33,3 @@ def test_query(subtests):
         with subtests.test(msg="manual stat"):
             # prove that we can use a plain string
             assert res.stat("read_ops") == 0
-
-
-def test_query_with_opts(
-    httpx_mock: HTTPXMock,
-    linearized: bool,
-    query_timeout_ms: int,
-    traceparent: str,
-    tags: Mapping[str, str],
-    max_contention_retries: int,
-):
-
-    def validate_headers(request: httpx.Request):
-        assert request.headers[Header.Linearized] == str(linearized).lower()
-        # being explicit to not figure out encoding a Mapping in the test
-        assert request.headers[Header.Tags] == "hello=world&testing=foobar"
-        assert request.headers[Header.TimeoutMs] == f"{query_timeout_ms}"
-        assert request.headers[Header.Traceparent] == traceparent
-        assert request.headers[Header.MaxContentionRetries] \
-            == f"{max_contention_retries}"
-
-        return httpx.Response(
-            status_code=200,
-            json={"data": "mocked"},
-        )
-
-    httpx_mock.add_callback(validate_headers)
-
-    with httpx.Client() as mockClient:
-        c = Client(http_client=HTTPXClient(mockClient))
-
-        res = c.query(
-            fql("not used, just sending to a mock client"),
-            opts=QueryOptions(
-                query_tags=tags,
-                linearized=linearized,
-                query_timeout_ms=query_timeout_ms,
-                traceparent=traceparent,
-                max_contention_retries=max_contention_retries,
-            ),
-        )
-
-        assert res.status_code == 200
