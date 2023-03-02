63c634246c1cd3a5346a4eb0a267fff635025c33
Darren Cunningham <darren.cunningham@fauna.com>
2023-03-02 17:18:15 -0500

    initialize client in subtests

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Mapping
+from typing import Dict, Mapping
 
 import httpx
 import pytest
@@ -207,7 +207,7 @@ def test_client_headers(
     subtests: pytest_subtests.SubTests,
     httpx_mock: HTTPXMock,
 ):
-    expected: Mapping[str, str] = {}
+    expected: Dict[str, str] = {}
 
     def validate_headers(request: httpx.Request):
         for header in expected:
@@ -221,17 +221,27 @@ def test_client_headers(
     httpx_mock.add_callback(validate_headers)
 
     with httpx.Client() as mockClient:
-        c = Client(http_client=HTTPXClient(mockClient))
+        http_client = HTTPXClient(mockClient)
+
+        with subtests.test("should allow for custom header on Client"):
+            expected = {"happy": "fox"}
+            c = Client(
+                http_client=http_client,
+                headers=expected,
+            )
+            c.query(fql("just a mock"))
 
         with subtests.test("should allow custom header"):
+            c = Client(http_client=http_client)
+            expected = {"yellow": "submarine"}
             c.query(
                 fql("just a mock"),
-                QueryOptions(headers={"yellow": "submarine"}),
+                QueryOptions(headers=expected),
             )
 
         with subtests.test("Linearized should be set on Client"):
             c = Client(
-                http_client=HTTPXClient(mockClient),
+                http_client=http_client,
                 linearized=True,
             )
             expected = {Header.Linearized: "true"}
@@ -239,6 +249,7 @@ def test_client_headers(
 
         with subtests.test("Linearized should be set on Query"):
             expected = {Header.Linearized: "true"}
+            c = Client(http_client=http_client)
             c.query(
                 fql("just a mock"),
                 QueryOptions(linearized=True),
@@ -248,7 +259,7 @@ def test_client_headers(
             count = 5
 
             c = Client(
-                http_client=HTTPXClient(mockClient),
+                http_client=http_client,
                 max_contention_retries=count,
             )
 
@@ -258,6 +269,7 @@ def test_client_headers(
         with subtests.test("Max Contention Retries on Query"):
             count = 5
             expected = {Header.MaxContentionRetries: f"{count}"}
+            c = Client(http_client=http_client)
             c.query(
                 fql("just a mock"),
                 QueryOptions(max_contention_retries=count),
@@ -267,6 +279,7 @@ def test_client_headers(
         with subtests.test("Should have a Traceparent"):
             traceparent = "moshi-moshi"
             expected = {Header.Traceparent: traceparent}
+            c = Client(http_client=http_client)
             c.query(
                 fql("just a mock"),
                 QueryOptions(traceparent=traceparent),
