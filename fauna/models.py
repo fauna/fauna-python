6462de655e0e40a4c3b1308327f7c6056f3e0402
Lucas Pedroza <lucas.pedroza@fauna.com>
2023-03-08 10:57:08 +0100

    Add a DocumentDict model for @doc protocol change

--- fauna/models.py
+++ fauna/models.py
@@ -1,25 +1,4 @@
-class DocumentReference:
-
-    def __init__(self, collection_name: str, ref_id: str):
-        self.collection = collection_name
-        self.id = ref_id
-
-    def __str__(self):
-        return f"{self.collection}:{self.id}"
-
-    def __eq__(self, other):
-        return isinstance(other, DocumentReference) and str(self) == str(other)
-
-    def __hash__(self):
-        hash((self.collection, self.id))
-
-    @staticmethod
-    def from_string(ref: str):
-        rs = ref.split(":")
-        if len(rs) != 2:
-            raise ValueError(
-                "Expects string of format <CollectionName>:<RefID>")
-        return DocumentReference(rs[0], rs[1])
+from typing import Union
 
 
 class Module:
@@ -35,3 +14,45 @@ class Module:
 
     def __hash__(self):
         hash(self.name)
+
+
+class DocumentReference:
+    collection: Module
+    ref_id: str
+
+    def __init__(self, collection: Union[str, Module], ref_id: str):
+        if isinstance(collection, str):
+            self.collection = Module(collection)
+        else:
+            self.collection = collection
+
+        self.ref_id = ref_id
+
+    def __str__(self):
+        return f"{self.collection}:{self.ref_id}"
+
+    def __eq__(self, other):
+        return isinstance(other, DocumentReference) and str(self) == str(other)
+
+    def __hash__(self):
+        hash((self.collection, self.ref_id))
+
+    @staticmethod
+    def from_string(ref: str):
+        rs = ref.split(":")
+        if len(rs) != 2:
+            raise ValueError(
+                "Expects string of format <CollectionName>:<RefID>")
+        return DocumentReference(rs[0], rs[1])
+
+
+class DocumentDict(dict):
+
+    def ref(self) -> DocumentReference:
+        for k in ["coll", "id"]:
+            if k not in self:
+                raise ValueError(
+                    f"Document does not contain the required '{k}' key to return a reference"
+                )
+
+        return DocumentReference(self["coll"], self["id"])
