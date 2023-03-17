9cafde781fdcdef8d95784e15c432091323444ab
Lucas Pedroza <40873230+pnwpedro@users.noreply.github.com>
2023-03-17 17:28:03 +0100

    Add composition integ test (#55)

--- tests/integration/test_query.py
+++ tests/integration/test_query.py
@@ -1,12 +1,11 @@
 import pytest
 
-from fauna import fql
+from fauna import fql, Document
 from fauna.errors import QueryRuntimeError, ConstraintFailure
 from fauna.response import Stat
 
 
 def test_query(subtests, client):
-
     with subtests.test(msg="valid query"):
         res = client.query(fql("Math.abs(-5.123e3)"))
 
@@ -49,3 +48,49 @@ def test_query_with_constraint_failure(client):
     assert e.value.code == "constraint_failure"
     assert e.value.message == "Failed to create document in collection Function."
     assert len(e.value.summary) > 0
+
+
+def test_advanced_composition(client, a_collection):
+    test_email = "foo@fauna.com"
+    index = {
+        "indexes": {
+            "byEmail": {
+                "terms": [{
+                    "field": "email"
+                }],
+                "values": [{
+                    "field": "email"
+                }]
+            }
+        }
+    }
+    client.query(
+        fql("${col}.definition.update(${idx})", col=a_collection, idx=index))
+    doc = client.query(
+        fql("${col}.create(${doc})",
+            col=a_collection,
+            doc={
+                "email": test_email,
+                "alias": "bar"
+            })).data
+
+    def doc_by_email(email: str):
+        return fql("${col}.byEmail(${email}).first()",
+                   col=a_collection,
+                   email=email)
+
+    def update_doc_by_email(email: str, data: dict):
+        q = """
+let u = ${user}
+u.update(${data})
+"""
+        return fql(q, user=doc_by_email(email), data=data)
+
+    result = client.query(update_doc_by_email(test_email, {"alias": "baz"}))
+
+    assert isinstance(result.data, Document)
+    assert result.data["email"] == test_email
+    assert result.data["alias"] == "baz"
+    assert result.data.id == doc.id
+    assert result.data.coll == doc.coll
+    assert result.data.ts != doc.ts
