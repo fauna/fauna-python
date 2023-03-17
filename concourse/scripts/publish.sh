255ff77d6a6002d7a40a59576d73e6206850186e
Darren Cunningham <darren.cunningham@fauna.com>
2023-03-17 13:17:27 -0400

    again

--- concourse/scripts/publish.sh
+++ concourse/scripts/publish.sh
@@ -7,6 +7,7 @@ cd ./fauna-python-repository
 PACKAGE_VERSION=$(python setup.py --version)
 echo "Going to publish python package: ${PACKAGE_VERSION}"
 
+pip install -r requirements.txt
 pip install twine
 
 twine check dist/*
