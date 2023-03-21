e284e42f0df67a2e64029ea173b6d8789b3f04f4
Darren Cunningham <darren.cunningham@fauna.com>
2023-03-21 08:32:42 -0400

    removed unused lines

--- concourse/scripts/integration-tests.sh
+++ concourse/scripts/integration-tests.sh
@@ -4,12 +4,6 @@ set -eou
 
 apk add --update make
 
-#pip install .
-#pip install nose2
-#pip install pylint
-#pip install requests
-#make test
-
 pip install -r requirements.txt
 pip install codecov
 coverage run setup.py test
