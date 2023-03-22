9eff4b2a7885cfcc34488b80d24203987f7bf609
Lucas Pedroza <40873230+pnwpedro@users.noreply.github.com>
2023-03-22 19:29:06 +0100

    Ping faunadb until it's ready (#72)

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,10 +2,25 @@
 
 set -eou
 
-apk add --update make
+apk add --update make curl
 
 pip install . .[test]
 pip install codecov
+
+attempt_counter=0
+max_attempts=100
+
+until $(curl -m 1 --output /dev/null --silent --head --fail $FAUNA_ENDPOINT/ping); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo ""
+    echo "Max attempts reached to $FAUNA_ENDPOINT/ping"
+    exit 1
+  fi
+
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 5
+done
 
 python -m coverage run -m pytest tests/unit
 python -m coverage run -m pytest tests/integration
