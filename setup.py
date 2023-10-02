1feaa1936282d012116b56fa5e4dfa43c49a2eb4
Darren Cunningham <darren.cunningham@fauna.com>
2023-10-02 11:25:23 -0400

    python 3.12

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Database",
         "Topic :: Database :: Front-Ends",
         "Operating System :: MacOS :: MacOS X",
